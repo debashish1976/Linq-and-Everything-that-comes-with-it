a87d7eae501bd74d0ab3b6295053619618bd99a9
Debashish Mishra <debashish.mishra@pwc.com>
2019-09-25 15:19:40 +0530

    Add project files.

---
+++ Capturing variables in a lambda expression/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Capturing_variables_in_a_lambda_expression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var demo = new CapturedVariablesDemo();
+            Action<string> action = demo.CreateAction("method argument");
+            action("lambda argument");
+        }
+    }
+    class CapturedVariablesDemo
+    {
+        private string instanceField = "instance field";
+
+        public Action<string> CreateAction(string methodParameter)
+        {
+            string methodLocal = "method local";
+            string uncaptured = "uncaptured local";
+
+            Action<string> action = lambdaParameter =>
+            {
+                string lambdaLocal = "lambda local";
+                Console.WriteLine("Instance field: {0}", instanceField);
+                Console.WriteLine("Method parameter: {0}", methodParameter);
+                Console.WriteLine("Method local: {0}", methodLocal);
+                Console.WriteLine("Lambda parameter: {0}", lambdaParameter);
+                Console.WriteLine("Lambda local: {0}", lambdaLocal);
+            };
+            methodLocal = "modified method local";
+            return action;
+        }
+    }
+}
---
+++ Instantiating a local variable multiple times/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Instantiating_a_local_variable_multiple_times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Action> actions = CreateActions();
+            foreach (Action action in actions)
+            {
+                action();
+            }
+        }
+        static List<Action> CreateActions()
+        {
+            List<Action> actions = new List<Action>();
+            for (int i = 0; i < 5; i++)
+            {
+                string text = string.Format("message {0}", i); //Declares a local variable within the loop
+                actions.Add(() => Console.WriteLine(text)); //Captures the variable in a lambda expression
+            }
+            return actions;
+        }
+    }
+}
